f52423ce47df52c6cbe7648819dc375fb72cdf89
Ryan Dabler <rpdabler@gmail.com>
2018-04-11 14:12:18 -0600

    add javascript scroll animation

---
+++ index.js
@@ -0,0 +1,19 @@
+const PADDING_OFFSET = 20;
+
+function scrollToElement(e) {
+    const targetId = e.currentTarget.dataset.target;
+    const target = document.getElementById(targetId);
+
+    const nav = document.getElementById("navigation");
+    const navOffset = nav.offsetHeight;
+    
+    window.scrollTo({
+        "behavior": "smooth",
+        "top": target.offsetTop - navOffset - PADDING_OFFSET
+    });
+}
+
+document.getElementById("hero-link").addEventListener("click", scrollToElement);
+document.getElementById("about-link").addEventListener("click", scrollToElement);
+document.getElementById("portfolio-link").addEventListener("click", scrollToElement);
+document.getElementById("contact-link").addEventListener("click", scrollToElement);
